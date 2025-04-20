d47e219679c23ef28699f5df61e8d09a6f45d4bb
Renabrasa <renato_alves09@hotmail.com>
2025-04-20 20:46:09 -0300

    ajuste do request no main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from models.models import db
 from routes.plantel import plantel_bp
 from routes.convidados import convidado_bp
