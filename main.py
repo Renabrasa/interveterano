82e3437b7569b248c272fa92dd85b98f5b2eab30
Renabrasa <renato_alves09@hotmail.com>
2025-05-04 23:16:28 -0300

    Adicionando main para trabalhar no banco neon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 from models.models import db
 from routes.plantel import plantel_bp
 from routes.convidados import convidado_bp
@@ -6,23 +6,22 @@ from routes.calendario import calendario_bp
 from routes.performance import performance_bp
 from routes.financeiro import financeiro_bp
 from routes.auth import auth_bp
-import os
 from routes.galeria import galeria_bp
 from sqlalchemy import text
 from routes.ajuste import ajuste_bp
 from routes.pessoas import pessoa_bp
-from routes.backup import backup_bp
+import os
 
 app = Flask(__name__)
 app.secret_key = 'inter-veterano-super-segura-2025'
 
 # ================================
-# CONFIGURAÇÃO PARA POSTGRESQL NO RENDER
+# CONFIGURAÇÃO PARA POSTGRESQL NO NEON
 # ================================
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'postgresql://interadmin:S12In5Kcd2ZDtCFLH177JUZ4cWIyi4DH'
-    '@dpg-d02n42adbo4c73f097sg-a/interveterano_db'
-)
+DATABASE_URL = os.getenv('DATABASE_URL')
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL não configurada nas variáveis de ambiente.")
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # ================================
@@ -31,8 +30,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 with app.app_context():
-    db.create_all() 
-    
+    db.create_all()
+
+# ================================
+# REGISTRO DOS BLUEPRINTS
+# ================================
 app.register_blueprint(plantel_bp)
 app.register_blueprint(convidado_bp)
 app.register_blueprint(calendario_bp)
@@ -42,7 +44,6 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(galeria_bp)
 app.register_blueprint(ajuste_bp)
 app.register_blueprint(pessoa_bp)
-app.register_blueprint(backup_bp)
 
 @app.route('/')
 def home():
@@ -51,8 +52,6 @@ def home():
 @app.errorhandler(502)
 def erro_502(e):
     return render_template('erro502.html'), 502
-
-
 
 # ================================
 # EXECUÇÃO LOCAL
