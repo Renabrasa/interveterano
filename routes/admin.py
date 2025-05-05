656ec0c1b9f17013fc255da4d5faffbe015894d8
Renabrasa <renato_alves09@hotmail.com>
2025-05-05 00:20:06 -0300

    admin2

--- routes/admin.py
+++ routes/admin.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
-from models.models import db, Pessoa, Performance, Mensalidade,Jogador  
+from models.models import db, Pessoa, Performance, Mensalidade
 
 
 
