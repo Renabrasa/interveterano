e5afbeb79721ec6bb76fd00840c15a8f81ae01cb
Renabrasa <renato_alves09@hotmail.com>
2025-05-06 17:28:42 -0300

    config

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'chave-secreta')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://rena1558_interadmin:Intervet2025@interveterano.com/rena1558_intervetdb'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://rena1558_interadmin:Intervet2025@br000.mysql.hostgator.com/rena1558_intervetdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
