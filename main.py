3cd805f12f3c48bc22043a4b8fb559891b542973
Renabrasa <renato_alves09@hotmail.com>
2025-05-06 17:05:12 -0300

    ajuste no yaml

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 from models.models import db
+from config import Config
+
+# Blueprints
 from routes.plantel import plantel_bp
 from routes.convidados import convidado_bp
 from routes.calendario import calendario_bp
@@ -10,19 +13,17 @@ from routes.galeria import galeria_bp
 from routes.pessoas import pessoa_bp
 from routes.admin import admin_bp
 
-
-import os
-
 app = Flask(__name__)
-app.secret_key = 'inter-veterano-super-segura-2025'  # ou qualquer string segura
+from config import Config
+app.config.from_object(Config)
 
-# Caminho absoluto para o banco SQLite na pasta 'instance'
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'instance', 'inter.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Inicializa banco e blueprint
+# Inicializa DB
 db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+# Registro de Blueprints
 app.register_blueprint(plantel_bp)
 app.register_blueprint(convidado_bp)
 app.register_blueprint(calendario_bp)
@@ -31,26 +32,17 @@ app.register_blueprint(financeiro_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(galeria_bp)
 app.register_blueprint(pessoa_bp)
-app.register_blueprint(admin_bp)
+app.register_blueprint(admin_bp, url_prefix='/admin')
 
-
-
-# Rota home
 @app.route('/')
 def home():
     return render_template('home.html')
-
-@app.route('/erro502')
-def simular_erro502():
-    from werkzeug.exceptions import BadGateway
-    raise BadGateway()
-
 
 @app.errorhandler(502)
 def erro_502(e):
     return render_template('erro502.html'), 502
 
-
-# Executa a aplicação
+# Para rodar localmente
 if __name__ == '__main__':
     app.run(debug=True)
+
