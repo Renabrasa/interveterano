4826b9af1491f7352cceee1bf851adba92698d71
Renabrasa <renato_alves09@hotmail.com>
2025-05-06 21:06:58 -0300

    Deploy inicial

--- verificar_dados.py
+++
@@ -1,7 +0,0 @@
-from main import app
-from models.models import db, ConfigFinanceiro
-
-with app.app_context():
-    if not ConfigFinanceiro.query.first():
-        db.session.add(ConfigFinanceiro(mensalidade_padrao=30.00))
-        db.session.commit()
--- verificar_rotas.py
+++
@@ -1,6 +0,0 @@
-from main import app
-
-with app.app_context():
-    print("\n📋 ROTAS REGISTRADAS NO FLASK:")
-    for rule in app.url_map.iter_rules():
-        print(f"{rule.methods} => {rule.rule}")
